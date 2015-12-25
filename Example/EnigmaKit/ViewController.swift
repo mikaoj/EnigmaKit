90806bd6742f29a3e8f22a8a8303d0ffbbef626f
Joakim Gyllstrom <joakim@backslashed.se>
2015-12-25 19:51:06 +0100

    Basic example

--- Example/EnigmaKit/ViewController.swift
+++ Example/EnigmaKit/ViewController.swift
@@ -1,16 +1,30 @@
+// The MIT License (MIT)
 //
-//  ViewController.swift
-//  EnigmaKit
+// Copyright (c) 2015 Joakim Gyllström
 //
-//  Created by Joakim Gyllstrom on 12/25/2015.
-//  Copyright (c) 2015 Joakim Gyllstrom. All rights reserved.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 import UIKit
 import EnigmaKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var label: UITextView!
     @IBOutlet weak var textField: UITextField!
     
     let enigma = Enigma()
@@ -20,11 +34,8 @@ class ViewController: UIViewController {
     }
     
     @IBAction func handleButtonPressed(sender: UIButton) {
-        if let text = textField.text {
-            label.text = enigma.encode(text)
-        } else {
-            label.text = ""
-        }
+        guard let text = textField.text else { label.text = ""; return }
+        label.text = enigma.encode(text)
     }
 }
 
